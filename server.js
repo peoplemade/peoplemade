e09a0d4233e406be739bd61d92b57389b8382353
AL3K2 <99705644+413K2@users.noreply.github.com>
2025-12-09 11:57:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import Hash from "./models/Hash.js";
+import Hash from "./Hash.js";
 
 dotenv.config();
 
