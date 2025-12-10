209d616d86f7c2c513ef01c8c4bb90211ae278a7
AL3K2 <99705644+413K2@users.noreply.github.com>
2025-12-09 22:23:12 -0600

    Delete Hash.js

--- Hash.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const HashSchema = new mongoose.Schema({
-    hash: { type: String, unique: true, required: true },
-    certificateID: { type: String, required: true },
-    date: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Hash", HashSchema);
